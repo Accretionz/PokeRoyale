5ae40fca7da171c08b1d2a174645dfad1e38ed56
Accretionz <jieliu415@gmail.com>
2024-10-11 00:25:25 -0700

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import opponentPokemonList from "./components/OpponentPokemon";
 import Title from "./components/Title";
 
 export default function App() {
-    const firstPokemon = opponentPokemonList.pokemon5.name;
+    const firstPokemon = opponentPokemonList.pokemon4.name;
     const firstMove = opponentPokemonList.pokemon1.moves1;
     return (
         <Router>
